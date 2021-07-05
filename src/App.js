519d5fc45d16f91eeef838abd4e76d5d54cf6829
madhumitamanivannane@pec.edu <madhumitamanivannane@pec.edu>
2021-07-05 11:01:56 -0700

    First Api Call

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import './App.css';
+import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
 
 import Weather from './app_component/weather.component';
 
+
+//api calls api.openweathermap.org/data/2.5/weather?q=London,uk&appid={API key}
 const API_key = '30e40bd1f8db652a7d54fdf4420d0844';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {};
+    this.getWeather();
+  }
+
+  getWeather = async()=>{
+    const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`);
+    const response = await api_call.json();
+    console.log(response);
+  };
+
+  render(){
+    return(
+      <div className="App">
       <Weather/>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
