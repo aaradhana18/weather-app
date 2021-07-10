d5df201e266efb13181a2a9c9b3a9592426969fb
madhumitamanivannane@pec.edu <madhumitamanivannane@pec.edu>
2021-07-10 11:36:14 -0700

    Final Commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
 
 import Weather from './app_component/weather.component';
+import Form from './app_component/form.component';
 
 
 //api calls api.openweathermap.org/data/2.5/weather?q=London,uk&appid={API key}
@@ -24,7 +25,7 @@ class App extends React.Component{
       description: "",
       error: false
     };
-    this.getWeather();
+    //this.getWeather();
 
     this.weatherIcon = {
       Thunderstorm:"wi-thunderstorm",
@@ -70,27 +71,38 @@ class App extends React.Component{
     }
   }
 
-  getWeather = async()=>{
-    //const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`);
-    //const api_call = await fetch(`http://worldtimeapi.org/api/timezone/Asia/Kolkata`);
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Washington,uk&appid=${API_KEY}`)
-    const response = await api_call.json();
-    console.log(response);
-    this.setState({
-      city:response.name,
-      country:response.sys.country,
-      celsius:this.calCelsius(response.main.temp),
-      temp_max:this.calCelsius(response.main.temp_max),
-      temp_min:this.calCelsius(response.main.temp_min),
-      description:response.weather[0].description,
-      //icon:this.weatherIcon.Thunderstorm
-    });
-    this.get_WeatherIcon(this.weatherIcon,response.weather[0].id);
+  getWeather = async(e)=>{
+
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    if(city && country){
+      //const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`);
+      //const api_call = await fetch(`http://worldtimeapi.org/api/timezone/Asia/Kolkata`);
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`)
+      const response = await api_call.json();
+      console.log(response);
+      this.setState({
+        city:`${response.name},${response.sys.country}`,
+        //country:response.sys.country,
+        celsius:this.calCelsius(response.main.temp),
+        temp_max:this.calCelsius(response.main.temp_max),
+        temp_min:this.calCelsius(response.main.temp_min),
+        description:response.weather[0].description,
+        //icon:this.weatherIcon.Thunderstorm
+      });
+      this.get_WeatherIcon(this.weatherIcon,response.weather[0].id);
+    }
+    else{
+      this.setState({error:true})
+    }
   };
 
   render(){
     return(
       <div className="App">
+      <Form loadweather = {this.getWeather} error ={this.state.error}/>
       <Weather 
       city={this.state.city} 
       country={this.state.country} 
--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Weather = (props) =>{
     return(
-        <div className="container">
-            <div className="cards">
-                <h1>{props.city},{props.country}</h1>
+        <div className="container text-light">
+            <div className="cards pt-4">
+                <h1>{props.city}</h1>
                 <h5 className="py-4"> 
                     <i className={`wi ${props.weatherIcon} display-1`}/>
                 </h5>
-                <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+                {props.temp_celsius?(<h1 className="py-2">{props.temp_celsius}&deg;</h1>):null}
                 {minmaxTemp(props.temp_min,props.temp_max)}
                 <h4 className="py-3">{props.description}</h4>
             </div>
@@ -16,13 +16,15 @@ const Weather = (props) =>{
     );
 };
 
-function minmaxTemp(_min,_max){
-    return(
-        <h3>
-            <span className="px-4">{_min}&deg;</span>
-            <span className="px-4">{_max}&deg;</span>
-        </h3>
-    );
+function minmaxTemp(min,max){
+    if(min && max){
+        return(
+            <h3>
+                <span className="px-4">{min}&deg;</span>
+                <span className="px-4">{max}&deg;</span>
+            </h3>
+        );
+    }
 }
 
 export default Weather;
